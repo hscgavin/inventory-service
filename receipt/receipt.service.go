73b60597d3fa659a55f8731912a4a1d795176fab
Gavin He <hscgavin@gmail.com>
2021-02-27 20:46:46 +1100

    Add download file handler

--- receipt/receipt.service.go
+++ receipt/receipt.service.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/hscgavin/inventory-service/cors"
 )
@@ -17,9 +19,39 @@ const receiptPath = "receipts"
 // SetupRoutes :
 func SetupRoutes(apiBasePath string) {
 	receiptHandler := http.HandlerFunc(handleReceipts)
+	downloadHandler := http.HandlerFunc(handleDownload)
 	http.Handle(fmt.Sprintf("%s/%s", apiBasePath, receiptPath), cors.Middleware(receiptHandler))
+	http.Handle(fmt.Sprintf("%s/%s/", apiBasePath, receiptPath), cors.Middleware(downloadHandler))
 }
 
+func handleDownload(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, fmt.Sprintf("%s/", receiptPath))
+	if len(urlPathSegments[1:]) > 1 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fileName := urlPathSegments[1:][0]
+	file, err := os.Open(filepath.Join(ReceiptDirectory, fileName))
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+	fHeader := make([]byte, 512)
+	file.Read(fHeader)
+	fContentType := http.DetectContentType(fHeader)
+	stat, err := file.Stat()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fSize := strconv.FormatInt(stat.Size(), 10)
+	w.Header().Set("Content-Disposition", "attachment; filename="+fileName)
+	w.Header().Set("Content-Type", fContentType)
+	w.Header().Set("Content-Length", fSize)
+	file.Seek(0, 0)
+	io.Copy(w, file)
+}
 func handleReceipts(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
