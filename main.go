3797637bcad0ec99e5f4a97fdbe694dddd6c50f0
Gavin He <hscgavin@gmail.com>
2021-02-27 12:04:14 +1100

    Refactor and update service

--- main.go
+++ main.go
@@ -3,24 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
 	"time"
-)
 
-// comment
-type Product struct {
-	ProductID      int    `json:"productId"`
-	Manufacturer   string `json:"manufacturer"`
-	Sku            string `json:"sku"`
-	Upc            string `json:"upc"`
-	PricePerUnit   string `json:"pricePerUnit"`
-	QuantityOnHand int    `json:"quantityOnHand"`
-	ProductName    string `json:"productName"`
-}
+	"github.com/hscgavin/inventory-service/product"
+)
 
 var productList []Product
 
@@ -74,85 +62,6 @@ func findProductByID(productID int) (*Product, int) {
 	return nil, 0
 }
 
-func productsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		productsJSON, err := json.Marshal(productList)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(productsJSON)
-	case http.MethodPost:
-		var newProduct Product
-		bodyBytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		err = json.Unmarshal(bodyBytes, &newProduct)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		if newProduct.ProductID != 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		newProduct.ProductID = getNextID()
-		productList = append(productList, newProduct)
-		w.WriteHeader(http.StatusCreated)
-		return
-	}
-}
-
-func productHandler(w http.ResponseWriter, r *http.Request) {
-	urlPathSegments := strings.Split(r.URL.Path, "products/")
-	productID, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	product, listItemIndex := findProductByID(productID)
-	if product == nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	switch r.Method {
-	case http.MethodGet:
-		productJSON, err := json.Marshal(product)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(productJSON)
-	case http.MethodPut:
-		var updatedProduct Product
-		bodyBytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		err = json.Unmarshal(bodyBytes, &updatedProduct)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		if updatedProduct.ProductID != productID {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		product = &updatedProduct
-		productList[listItemIndex] = *product
-		w.WriteHeader(http.StatusOK)
-		return
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
-}
-
 func middlewareHandler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("before handler; middleware start")
@@ -162,12 +71,9 @@ func middlewareHandler(handler http.Handler) http.Handler {
 	})
 }
 
+const apiBasePath = "/api"
+
 func main() {
-
-	productListHandler := http.HandlerFunc(productsHandler)
-	productItemHandler := http.HandlerFunc(productHandler)
-
-	http.Handle("/products", middlewareHandler(productListHandler))
-	http.Handle("/products/", middlewareHandler(productItemHandler))
+	product.SetupRoutes(apiBasePath)
 	http.ListenAndServe("127.0.0.1:5000", nil)
 }
---
+++ product/product.service.go
@@ -0,0 +1,105 @@
+package product
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+const productsBasePath = "products"
+
+func SetupRoutes(apiBasePath string) {
+	handleProducts := http.HandlerFunc(productsHandler)
+	handleProduct := http.HandlerFunc(productHandler)
+	http.Handle(fmt.Sprintf("%s/%s", apiBasePath, productsBasePath), handleProducts)
+	http.Handle(fmt.Sprintf("%s/%s", apiBasePath, productsBasePath), handleProduct)
+}
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productList := getProductList()
+		productsJSON, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJSON)
+	case http.MethodPost:
+		var newProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &newProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if newProduct.ProductID != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		_, err = addOrUpdateProduct(newProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, "products/")
+	productID, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	product := getProduct(productID)
+	if product == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+		productJSON, err := json.Marshal(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productJSON)
+	case http.MethodPut:
+		var updatedProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &updatedProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if updatedProduct.ProductID != productID {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		_, err = addOrUpdateProduct(updatedProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
